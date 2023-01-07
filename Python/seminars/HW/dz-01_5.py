63bcb5ffb1b40938f9c62d8025b3ad3d4e17dfbb
Sorvic_mbp-st <sorvic@gmail.com>
2023-01-07 21:46:58 +0300

    add description HW

---
+++ Python/seminars/HW/dz-01_5.py
@@ -0,0 +1,10 @@
+# 5. Напишите программу, которая принимает на вход координаты
+# двух точек и находит расстояние между ними в 2D пространстве.
+# https://ru.onlinemschool.com/math/library/analytic_geometry/point_point_length/
+# in
+# - 3
+# - 6
+# - 2
+# - 1
+# out
+# 5.099
